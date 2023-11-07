72c0f0851a4c181025188cf46325adc49bd7219f
Bjørnar H. Langved <b.langved@icloud.com>
2023-11-07 19:17:31 +0100

    cypress.config fix
    Hoping this will fix the error creating a pull request

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // BASE_URL is defined in cypress.env.json, use it. Otherwise, use the default.
-      config.baseUrl = config.env.BASE_URL || "http://localhost:5500/";
+      config.baseUrl = config.env.BASE_URL;
       return config;
     },
     defaultCommandTimeout: 10000,
